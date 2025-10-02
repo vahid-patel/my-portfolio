dd6c457e8096dd0f07b21e9e9161eddb48c2a568
vahid-patel <144371170+vahid-patel@users.noreply.github.com>
2025-10-02 18:59:38 +0530

    Initialised

---
+++ deskop-portfolio/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <Image src={'/kali-metal-dark.png'} alt="WallPaper" fill className="object-cover object-center"/>
+    </>
+  );
+}
